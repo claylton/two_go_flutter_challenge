f4dc428e13631662ca0f4dcce868f153da76ad87
Claylton Santos <clayltonsp@hotmail.com>
2024-09-16 20:18:20 -0300

    add mock and product model

--- lib/model/product_model.dart
+++ lib/model/product_model.dart
@@ -1,27 +1,43 @@
 class ProductItemModel {
+  final String id;
   final String imagePath;
   final String title;
+  String discountInfo;
+  int discountType;
   final double price;
+  int quantity;
 
-  const ProductItemModel({
+  ProductItemModel({
+    required this.id,
     required this.imagePath,
     required this.title,
+    required this.discountInfo,
+    required this.discountType,
     required this.price,
+    required this.quantity,
   });
 
   factory ProductItemModel.fromJson(Map<String, dynamic> json) {
     return ProductItemModel(
+      id: json['id'],
       imagePath: json['imagePath'],
       title: json['title'],
+      discountInfo: json['discountInfo'],
+      discountType: json['discountType'],
       price: json['price'].toDouble(),
+      quantity: json['quantity'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'imagePath': imagePath,
       'title': title,
+      'discountInfo': discountInfo,
+      'discountType': discountType,
       'price': price,
+      'quantity': quantity,
     };
   }
 }
--- lib/services/mocks/mock_products.dart
+++ lib/services/mocks/mock_products.dart
@@ -5,29 +5,49 @@ abstract class ProductsMock {
       'message': 'success',
       'data': [
         {
+          "id": "1",
           "imagePath": "https://raw.githubusercontent.com/claylton/two_go_flutter_challenge/main/assets/product-10.png",
           "title": "Item A",
+          "discountInfo": "",
+          "discountType": 0,
           "price": 50.00,
+          "quantity": 0
         },
         {
+          "id": "2",
           "imagePath": "https://raw.githubusercontent.com/claylton/two_go_flutter_challenge/main/assets/product-1.png",
           "title": "Item B",
+          "discountInfo": "2 por R\$1,25",
+          "discountType": 1,
           "price": 75.00,
+          "quantity": 0
         },
         {
+          "id": "3",
           "imagePath": "https://raw.githubusercontent.com/claylton/two_go_flutter_challenge/main/assets/product-2.png",
           "title": "Item C",
+          "discountInfo": "Compre 3, leve um grátis",
+          "discountType": 2,
           "price": 25.00,
+          "quantity": 0
         },
         {
+          "id": "4",
           "imagePath": "https://raw.githubusercontent.com/claylton/two_go_flutter_challenge/main/assets/product-4.png",
           "title": "Item D",
+          "discountInfo": "Compre D e E por R\$ 3",
+          "discountType": 3,
           "price": 150.00,
+          "quantity": 0
         },
         {
+          "id": "5",
           "imagePath": "https://raw.githubusercontent.com/claylton/two_go_flutter_challenge/main/assets/product-5.png",
           "title": "Item E",
+          "discountInfo": "Compre D e E por R\$ 3",
+          "discountType": 4,
           "price": 200.00,
+          "quantity": 0
         },
       ],
     };
