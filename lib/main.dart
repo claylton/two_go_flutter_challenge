16feb922a763ad3448c17be42c6f600b747dd636
Claylton Santos <clayltonsp@hotmail.com>
2024-09-16 20:19:54 -0300

    Add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:two_go_flutter_challenge/blocs/cart_bloc.dart';
+import 'package:two_go_flutter_challenge/blocs/home_bloc.dart';
 import 'package:two_go_flutter_challenge/ui/themes/app_theme.dart';
 import 'ui/pages/tabs_page.dart';
 
@@ -8,6 +11,21 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<HomeBloc>.value(value: HomeBloc()),
+        ChangeNotifierProvider<CartBloc>.value(value: CartBloc()),
+      ],
+      child: const Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
